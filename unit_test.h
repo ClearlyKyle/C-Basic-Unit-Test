23f4e7a0013a251535b1022e24b7d8b9b2c11866
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-11-05 22:42:15 +0000

    updated for MSVC

--- unit_test.h
+++ unit_test.h
@@ -8,12 +8,29 @@ void globals_list_add(void (*f)(void), char *func_name, int line);
 void global_run_all_tests(void);
 void set_fail_message(char *msg, const char *func_name, int line);
 
-#define ADD_FUNCTION(f)                                            \
-    static void f(void);                                           \
-    static void __attribute__((constructor)) __construct_##f(void) \
-    {                                                              \
-        globals_list_add(f, #f, __LINE__);                         \
-    }                                                              \
+#if defined(_MSC_VER)
+#pragma section(".CRT$XCU", read)
+#define INITIALIZER2_(f, p)                                  \
+    static void f(void);                                     \
+    __declspec(allocate(".CRT$XCU")) void (*f##_)(void) = f; \
+    __pragma(comment(linker, "/include:" p #f "_")) static void f(void)
+#ifdef _WIN64
+#define INITIALIZER(f) INITIALIZER2_(f, "")
+#else
+#define INITIALIZER(f) INITIALIZER2_(f, "_")
+#endif
+#else
+#define INITIALIZER(f)                                \
+    static void f(void) __attribute__((constructor)); \
+    static void f(void)
+#endif
+
+#define ADD_FUNCTION(f)                    \
+    static void f(void);                   \
+    INITIALIZER(__construct_f)             \
+    {                                      \
+        globals_list_add(f, #f, __LINE__); \
+    }                                      \
     static void f(void)
 
 #define RUN_ALL_TESTS()                    \
@@ -51,9 +68,9 @@ void set_fail_message(char *msg, const char *func_name, int line);
         }                                                                                                                \
     } while (0)
 
-#define TEST_EQUAL_INT(expected, actual, message) __TEST_EQUALITY(message, "%d", expected, actual, __FUNCTION__, __LINE__)
+#define TEST_EQUAL_INT(expected, actual, message)   __TEST_EQUALITY(message, "%d", expected, actual, __FUNCTION__, __LINE__)
 #define TEST_EQUAL_FLOAT(expected, actual, message) __TEST_EQUALITY(message, "%f", expected, actual, __FUNCTION__, __LINE__)
-#define TEST_EQUAL_UINT(expected, actual, message) __TEST_EQUALITY(message, "%d", expected, actual, __FUNCTION__, __LINE__)
-#define TEST_EQUAL_CHAR(expected, actual, message) __TEST_EQUALITY(message, "%c", expected, actual, __FUNCTION__, __LINE__)
+#define TEST_EQUAL_UINT(expected, actual, message)  __TEST_EQUALITY(message, "%d", expected, actual, __FUNCTION__, __LINE__)
+#define TEST_EQUAL_CHAR(expected, actual, message)  __TEST_EQUALITY(message, "%c", expected, actual, __FUNCTION__, __LINE__)
 
 #endif // __UNIT_TEST_H__
